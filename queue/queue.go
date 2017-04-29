7d6830a2b0beb0ead8c6f1f06c3a4b2bf15997bc
Cesar Gimenes <crg@crg.eti.br>
2017-04-29 19:04:55 -0300

    queue package draft

--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,34 @@
 package queue
+
+import "sync"
+
+// Item struct is the basic queue item
+type Item struct {
+	Tag   string `json:"tag"`
+	Value []byte `json:"value"`
+}
+
+// Data contains all data in the current queue
+type Data struct {
+	Tag      string          `json:"tag"`
+	ItemList map[string]Item `json:"item"`
+	control  queueControl
+}
+
+type queueControl struct {
+	mutex *sync.Mutex
+}
+
+// New queue
+func New() (q *Data, err error) {
+	q = &Data{
+		ItemList: make(map[string]Item),
+		control:  queueControl{mutex: &sync.Mutex{}},
+	}
+	return
+}
+
+// Count items in the queue
+func (q *Data) Count() int {
+	return len(q.ItemList)
+}
