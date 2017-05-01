878d1e7b9a0d03b678a1dd89af4c4524cef51b28
Cesar Gimenes <crg@crg.eti.br>
2017-05-01 09:54:29 -0300

    Put function implemented

--- queue/queue.go
+++ queue/queue.go
@@ -1,6 +1,9 @@
 package queue
 
-import "sync"
+import (
+	"crypto/rand"
+	"sync"
+)
 
 // Item struct is the basic queue item
 type Item struct {
@@ -31,4 +34,24 @@ func New() (q *Data, err error) {
 // Count items in the queue
 func (q *Data) Count() int {
 	return len(q.ItemList)
+}
+
+// Put data in the queue
+func (q *Data) Put(b []byte) {
+	q.ItemList[randStr()] = Item{Value: b}
+}
+
+func randStr() (ret string) {
+	const charList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	const clLen byte = 62
+	const size = 36
+	var bytes = make([]byte, size)
+	if _, err := rand.Read(bytes); err != nil {
+		panic(err.Error())
+	}
+	for k, v := range bytes {
+		bytes[k] = charList[v%clLen]
+	}
+	ret = string(bytes)
+	return
 }
